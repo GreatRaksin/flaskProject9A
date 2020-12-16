79485e34453887206c0e19aaa1670aa85feeb813
demidraksin <greatraksin@icloud.com>
2020-12-16 10:57:50 +0300

    Adding first route

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def main_page():
+    return '<h1>Welcome to my site!</h1>'
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
